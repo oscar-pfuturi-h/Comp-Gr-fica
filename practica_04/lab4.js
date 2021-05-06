dd5b9477fe6cc206ddfc67c6fbdb9106cf499600
oscar-pfuturi-h <69215265+oscar-pfuturi-h@users.noreply.github.com>
2021-05-06 00:51:03 -0500

    update sys

--- practica_04/lab4.js
+++ practica_04/lab4.js
@@ -8,7 +8,6 @@ class Sphere {
         this.z = z;
         this.geometry = new THREE.SphereGeometry(this.r, this.a, this.t);
         this.setTexture(textures);
-        //this.material = new THREE.MeshPhongMaterial({color:0x00ff00, wireframe: true});
         this.mesh = new THREE.Mesh(this.geometry, this.material);
         this.mesh.position.set(this.x, this.y, this.z);
     }
@@ -20,7 +19,7 @@ class Sphere {
     }
     setTexture(textures) { // creamos el material con una(s) textura(s)
         if (textures == null) {
-            this.material = new THREE.MeshPhongMaterial({color:0x00ff00, wireframe: true});
+            this.material = new THREE.MeshBasicMaterial({color:0x00ff00, wireframe: true});
         }
         else {
             var texture = new THREE.TextureLoader().load(textures[0]);
@@ -54,7 +53,7 @@ class Planet extends Sphere {
     }
     move() {
         var time = Date.now();
-        this.x = Math.cos(time * (1.0 / (this.orbitRate * orbitData.value)) + 10.0) * this.distance;
+        this.x = -Math.cos(time * (1.0 / (this.orbitRate * orbitData.value)) + 10.0) * this.distance;
         this.z = Math.sin(time * (1.0 / (this.orbitRate * orbitData.value)) + 10.0) * this.distance;
         this.mesh.position.set(this.x, 0, this.z);
         this.mesh.rotation.y += this.rotationRate;
@@ -68,43 +67,7 @@ class Planet extends Sphere {
 
 var globalRate = 1.0;
 
-function setPlanetData(myOrbitRate, myRotationRate, myXDistance, myName, myTexture, myRadius, mySegments) {
-    return {
-        orbitRate: myOrbitRate,
-        rotationRate: myRotationRate,
-        distance: myXDistance,
-        name: myName,
-        texture: myTexture,
-        size: myRadius,
-        segments: mySegments
-    };
-}
-
-var earthData = setPlanetData(365.2564, 0.015, 85, "earth", "textures/earth.jpg", 1, 32);
-var moonData = setPlanetData(29.5, 0.01, 2.8, "moon", "textures/moon.jpg", 0.5, 32);
 var orbitData = {value: 200, runOrbit: true, runRotation: true};
-
-
-function movePlanet(myPlanet, myData, myTime, stopRotation) {
-    if (orbitData.runRotation && !stopRotation) {
-        myPlanet.rotation.y += myData.rotationRate;
-    }
-    if (orbitData.runOrbit) {
-        myPlanet.position.x = Math.cos(myTime 
-                * (1.0 / (myData.orbitRate * orbitData.value)) + 10.0) 
-                * myData.distanceFromAxis;
-        myPlanet.position.z = Math.sin(myTime 
-                * (1.0 / (myData.orbitRate * orbitData.value)) + 10.0) 
-                * myData.distanceFromAxis;
-    }
-}
-function moveMoon(moon, planetPosition) {
-    moon.move();
-    if (orbitData.runOrbit) {
-        moon.mesh.position.x = moon.mesh.position.x + planetPosition.x;
-        moon.mesh.position.z = moon.mesh.position.z + planetPosition.z;
-    }
-}
 
 // sol
 const sunRadius = 348.0; // reducido 1/2000
@@ -118,17 +81,19 @@ sun.position.set(0, 0, 0);
 // reduccion de la distancia 1/1000000
 // reduccion del radio 1/1000
 var mercury = new Planet(2.4, 12, 12, sunRadius + 57.90, 0, 0, ['textures/mercury.jpg', 'textures/mercurybump.jpg', null], 
-                        'Mercurio', 365.2564, 0.015, 0, sunRadius + 57.90);
+                        'Mercurio', 58.64, 0.015, 0, sunRadius + 57.90);
 var venus = new Planet(6.0, 32, 32, sunRadius + 108.2, 0, 0, ['textures/venus.jpg', 'textures/venusbump.jpg', null], 
-                        'Venus', 365.2564, 0.015, 0, sunRadius + 108.2);
-var earth = new Planet(6.3, 32, 32, sunRadius+149.6, 0, 0, ['textures/earth.jpg', 'textures/earthbump.jpg','textures/earthspecular.jpg'],                       'Tierra', 365.2564, 0.015, 1, sunRadius + 149.6);
-var moon = new Planet(1.7, 10, 10, 10.0, 0, 0, ['textures/moon.jpg', null, null], 'Luna', 29.5, 0.01, 0, 10.0);
+                        'Venus', 73.55, 0.015, 0, sunRadius + 108.2);
+var earth = new Planet(6.3, 32, 32, sunRadius+149.6, 0, 0, ['textures/earth.jpg', 'textures/earthbump.jpg','textures/earthspecular.jpg'],                       'Tierra', 104.115, 0.015, 1, sunRadius + 149.6);
+var moon = new Planet(1.7, 10, 10, 10.0, 0, 0, ['textures/moon.jpg', null, null], 'Luna', 3.5, 0.01, 0, 10.0);
 
 var mars = new Planet(3.3, 24, 24, sunRadius + 227.9, 0, 0, ['textures/mars.jpg', 'textures/marsbump.jpg', null], 
-                        'Marte', 365.2564, 0.015, 1, sunRadius + 227.9);
+                        'Marte', 141.05, 0.015, 1, sunRadius + 227.9);
 // reduccion de la distancia al sol 1/2000000
 var jupiter = new Planet(69.9, 64, 64, sunRadius + 389.2, 0, 0, ['textures/jupiter.jpg', null, null], 
-                        'Júpiter', 365.2564, 0.015, 5, sunRadius + 389.2);
+                        'Júpiter', 260.6, 0.015, 5, sunRadius + 389.2);
+var saturno = new Planet(58.2, 48, 48, sunRadius + 717.0, 0, 0, ['textures/saturn.jpg', null, null],
+                        'Saturno', 321.73, 0.015, 2, sunRadius + 717.0);
 
 
 
@@ -138,12 +103,9 @@ var camera = new THREE.PerspectiveCamera(
     0.1, // near
     1000 // far
 );
-camera.useQuaternion = true;
 camera.position.set(500, 500, 500);
 
-var quaternion = new THREE.Quaternion();
 const axis = new THREE.Vector3(0, 1, 0);
-
 
 const lightA = new THREE.AmbientLight(0x101010);
 
@@ -157,7 +119,8 @@ scene.add(venus.mesh);
 scene.add(earth.mesh);
 scene.add(moon.mesh);
 scene.add(mars.mesh);
-scene.add(jupiter.mesh)
+scene.add(jupiter.mesh);
+scene.add(saturno.mesh);
 
 const color = 0xFFFFFF;
 const intensity = 1.0;
@@ -178,7 +141,6 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-//renderer.render( scene, camera );
 renderer.render( scene, camera );
 
 {
@@ -205,7 +167,7 @@ renderer.render( scene, camera );
 // para los controles del mouse
 var controls = new THREE.OrbitControls( camera, renderer.domElement );
 controls.minDistance = 3; // minima distancia a q puede hacer zoom
-controls.maxDistance = 1000; // maxima distancia a q puede hacer zoom
+controls.maxDistance = 2000; // maxima distancia a q puede hacer zoom
 
 
 var animate = function(){
@@ -215,14 +177,15 @@ var animate = function(){
         camera.aspect = canvas.clientWidth / canvas.clientHeight;
         camera.updateProjectionMatrix();
       }
-    // para recorrer cada objeto de la scena (tambien incluye la camara)
-    sun.rotation.y += 0.001;
+
+    sun.rotation.y += 0.0001;
     mercury.move();
     venus.move();
     earth.move();
     moon.moveMoon(earth.mesh.position);
     mars.move();
     jupiter.move();
+    saturno.move();
 
     //circle.rotation 
     renderer.render( scene, camera );
