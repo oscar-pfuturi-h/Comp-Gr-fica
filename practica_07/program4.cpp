8886594c7e04f8f7356d711ba9f39cc09891d05f
oscar-pfuturi-h <69215265+oscar-pfuturi-h@users.noreply.github.com>
2021-06-01 13:25:59 -0500

    Create program4.cpp

---
+++ practica_07/program4.cpp
@@ -0,0 +1,150 @@
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <cstring>
+using namespace std;
+
+char* vertShaderPath = "/home/hermogene/Documents/ComputacionGrafica/opengl/src/04_shader_file/vertShader.glsl";
+char* fragShaderPath = "/home/hermogene/Documents/ComputacionGrafica/opengl/src/04_shader_file/fragShader.glsl";
+#define numVAOs 1
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+string readShaderSource(const char *filePath) {
+	string content;
+	ifstream fileStream(filePath, ios::in);
+	string line = "";
+	while (!fileStream.eof()) {
+			getline(fileStream, line);
+			content.append(line + "\n");
+	}
+	fileStream.close();
+	return content;
+}
+
+//////////////////////
+// functions to catch errors in GLSL
+//////////////////////
+void printShaderLog(GLuint shader) {
+	int len = 0;
+	int chWrittn = 0;
+	char *log;
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+	if (len > 0) {
+			log = (char *)malloc(len);
+			glGetShaderInfoLog(shader, len, &chWrittn, log);
+			cout << "Shader Info Log: " << log << endl;
+			free(log);
+	}
+}
+void printProgramLog(int prog) {
+	int len = 0;
+	int chWrittn = 0;
+	char *log;
+	glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &len);
+	if (len > 0) {
+			log = (char *)malloc(len);
+			glGetProgramInfoLog(prog, len, &chWrittn, log);
+			cout << "Program Info Log: " << log << endl;
+			free(log);
+	}
+}
+bool checkOpenGLError() {
+	bool foundError = false;
+	int glErr = glGetError();
+	while (glErr != GL_NO_ERROR) {
+			cout << "glError: " << glErr << endl;
+			foundError = true;
+			glErr = glGetError();
+	}
+	return foundError;
+}
+
+GLuint createShaderProgram() {
+	GLint vertCompiled;
+	GLint fragCompiled;
+	GLint linked;	
+
+	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	// read shaders from files		
+	string vertShaderStr = readShaderSource(vertShaderPath);
+	string fragShaderStr = readShaderSource(fragShaderPath);
+	const char *vertShaderSrc = vertShaderStr.c_str();
+	const char *fragShaderSrc = fragShaderStr.c_str();
+	glShaderSource(vShader, 1, &vertShaderSrc, NULL);
+	glShaderSource(fShader, 1, &fragShaderSrc, NULL);
+
+	glCompileShader(vShader);
+	checkOpenGLError();
+	glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);
+	if (vertCompiled != 1) {
+			cout << "vertex compilation failed" << endl;
+			printShaderLog(vShader);
+	}
+
+	glCompileShader(fShader);
+	checkOpenGLError();
+	glGetShaderiv(fShader, GL_COMPILE_STATUS, &fragCompiled);
+	if (fragCompiled != 1) {
+			cout << "fragment compilation failed" << endl;
+			printShaderLog(fShader);
+	}
+
+
+	GLuint vfProgram = glCreateProgram();
+	glAttachShader(vfProgram, vShader);
+	glAttachShader(vfProgram, fShader);
+
+	glLinkProgram(vfProgram);
+	checkOpenGLError();
+	glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);
+	if (linked != 1) {
+			cout << "linking failed" << endl;
+			printProgramLog(vfProgram);
+	}
+
+	return vfProgram;
+}
+
+void init(GLFWwindow* window) {
+	renderingProgram = createShaderProgram();
+	glGenVertexArrays(numVAOs, vao);
+	glBindVertexArray(vao[0]);
+}
+
+void display(GLFWwindow* window, double currentTime) {
+	glUseProgram(renderingProgram);
+	glPointSize(400.0f); // un vertice es un pixel, con esto especificamos el tamanio del pixel
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); //wire frame
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // noraml
+	glDrawArrays(GL_POINTS, 0, 1);
+}
+
+int main(void) {
+	if (!glfwInit()) { exit(EXIT_FAILURE); }
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter2 - program1", NULL, NULL);
+	glfwMakeContextCurrent(window);
+
+	//if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){ exit(EXIT_FAILURE); }
+
+	glfwSwapInterval(1);
+	init(window);
+	while (!glfwWindowShouldClose(window)) {
+			display(window, glfwGetTime());
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS); 
+}
