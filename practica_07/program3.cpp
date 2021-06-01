b09cd7d2e72fee0cebc3526a0aa6c4b4b43f5f4c
oscar-pfuturi-h <69215265+oscar-pfuturi-h@users.noreply.github.com>
2021-06-01 13:25:17 -0500

    Create program3.cpp

---
+++ practica_07/program3.cpp
@@ -0,0 +1,144 @@
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
+#include <iostream>
+
+using namespace std;
+
+
+#define numVAOs 1
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+
+// functions to catch errors in GLSL
+
+void printShaderLog(GLuint shader) {
+	int len = 0;
+	int chWrittn = 0;
+	char *log;
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+	if (len > 0) {
+			log = (char *)malloc(len);
+			glGetShaderInfoLog(shader, len, &chWrittn, log);
+			cout << "Shader Info Log: " << log << endl;
+			free(log);
+	}
+}
+void printProgramLog(int prog) {
+	int len = 0;
+	int chWrittn = 0;
+	char *log;
+	glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &len);
+	if (len > 0) {
+			log = (char *)malloc(len);
+			glGetProgramInfoLog(prog, len, &chWrittn, log);
+			cout << "Program Info Log: " << log << endl;
+			free(log);
+	}
+}
+bool checkOpenGLError() {
+	bool foundError = false;
+	int glErr = glGetError();
+	while (glErr != GL_NO_ERROR) {
+			cout << "glError: " << glErr << endl;
+			foundError = true;
+			glErr = glGetError();
+	}
+	return foundError;
+}
+//////////////////////
+//////////////////////
+
+GLuint createShaderProgram() {
+	GLint vertCompiled;
+	GLint fragCompiled;
+	GLint linked;
+
+	const char *vshaderSource =
+			"#version 430 \n"
+			"void main(void) \n"
+			"{ gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }";
+	// creamos un vertice, no especificamos salida, porque gl_position es por defecto de salida
+	const char *fshaderSource =
+			"#version 430 \n"
+			"out vec4 color; \n"
+			"void main(void) \n"
+			"{ if (gl_FragCoord.x < 200) color = vec4(1.0, 0.0, 0.0, 1.0); else color = vec4(0.0, 0.0, 1.0, 1.0); }";		
+	//especificamos el color de los pixeles
+
+	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(vShader, 1, &vshaderSource, NULL);
+	glShaderSource(fShader, 1, &fshaderSource, NULL);
+
+	glCompileShader(vShader);
+	checkOpenGLError();
+	glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);
+	if (vertCompiled != 1) {
+			cout << "vertex compilation failed" << endl;
+			printShaderLog(vShader);
+	}
+
+	glCompileShader(fShader);
+	checkOpenGLError();
+	glGetShaderiv(fShader, GL_COMPILE_STATUS, &fragCompiled);
+	if (fragCompiled != 1) {
+			cout << "fragment compilation failed" << endl;
+			printShaderLog(fShader);
+	}
+
+
+	GLuint vfProgram = glCreateProgram();
+	glAttachShader(vfProgram, vShader);
+	glAttachShader(vfProgram, fShader);
+
+	glLinkProgram(vfProgram);
+	checkOpenGLError();
+	glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);
+	if (linked != 1) {
+			cout << "linking failed" << endl;
+			printProgramLog(vfProgram);
+	}
+
+	return vfProgram;
+}
+
+void init(GLFWwindow* window) {
+	renderingProgram = createShaderProgram();
+	glGenVertexArrays(numVAOs, vao);
+	glBindVertexArray(vao[0]);
+}
+
+void display(GLFWwindow* window, double currentTime) {
+	glUseProgram(renderingProgram);
+	glPointSize(400.0f); // un vertice es un pixel, con esto especificamos el tamanio del pixel
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); //wire frame
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // noraml
+	glDrawArrays(GL_POINTS, 0, 1);
+
+}
+
+int main(void) {
+	if (!glfwInit()) { exit(EXIT_FAILURE); }
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter2 - program1", NULL, NULL);
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){ exit(EXIT_FAILURE); }
+
+	glfwSwapInterval(1);
+	init(window);
+	while (!glfwWindowShouldClose(window)) {
+			display(window, glfwGetTime());
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+}
