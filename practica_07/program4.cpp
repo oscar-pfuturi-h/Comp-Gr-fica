0d1b341cf11a06b64c1d0cce4d441c1b9df15a45
oscar-pfuturi-h <69215265+oscar-pfuturi-h@users.noreply.github.com>
2021-06-14 22:34:08 -0500

    glsl files

--- practica_07/program4.cpp
+++ practica_07/program4.cpp
@@ -3,23 +3,23 @@
 
 #include <iostream>
 #include <fstream>
-#include <unistd.h>
-#include <cstring>
+#include <string>
+
 using namespace std;
 
-char* vertShaderPath = "/home/hermogene/Documents/ComputacionGrafica/opengl/src/04_shader_file/vertShader.glsl";
-char* fragShaderPath = "/home/hermogene/Documents/ComputacionGrafica/opengl/src/04_shader_file/fragShader.glsl";
+char vertShaderPath[20] = "../vertShader.glsl";
+char fragShaderPath[20] = "../fragShader.glsl";
 #define numVAOs 1
 GLuint renderingProgram;
 GLuint vao[numVAOs];
 
-string readShaderSource(const char *filePath) {
+string readShaderSource(const char* filePath) {
 	string content;
 	ifstream fileStream(filePath, ios::in);
 	string line = "";
 	while (!fileStream.eof()) {
-			getline(fileStream, line);
-			content.append(line + "\n");
+		getline(fileStream, line);
+		content.append(line + "\n");
 	}
 	fileStream.close();
 	return content;
@@ -31,34 +31,34 @@ string readShaderSource(const char *filePath) {
 void printShaderLog(GLuint shader) {
 	int len = 0;
 	int chWrittn = 0;
-	char *log;
+	char* log;
 	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
 	if (len > 0) {
-			log = (char *)malloc(len);
-			glGetShaderInfoLog(shader, len, &chWrittn, log);
-			cout << "Shader Info Log: " << log << endl;
-			free(log);
+		log = (char*)malloc(len);
+		glGetShaderInfoLog(shader, len, &chWrittn, log);
+		cout << "Shader Info Log: " << log << endl;
+		free(log);
 	}
 }
 void printProgramLog(int prog) {
 	int len = 0;
 	int chWrittn = 0;
-	char *log;
+	char* log;
 	glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &len);
 	if (len > 0) {
-			log = (char *)malloc(len);
-			glGetProgramInfoLog(prog, len, &chWrittn, log);
-			cout << "Program Info Log: " << log << endl;
-			free(log);
+		log = (char*)malloc(len);
+		glGetProgramInfoLog(prog, len, &chWrittn, log);
+		cout << "Program Info Log: " << log << endl;
+		free(log);
 	}
 }
 bool checkOpenGLError() {
 	bool foundError = false;
 	int glErr = glGetError();
 	while (glErr != GL_NO_ERROR) {
-			cout << "glError: " << glErr << endl;
-			foundError = true;
-			glErr = glGetError();
+		cout << "glError: " << glErr << endl;
+		foundError = true;
+		glErr = glGetError();
 	}
 	return foundError;
 }
@@ -66,7 +66,7 @@ bool checkOpenGLError() {
 GLuint createShaderProgram() {
 	GLint vertCompiled;
 	GLint fragCompiled;
-	GLint linked;	
+	GLint linked;
 
 	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
 	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -74,8 +74,8 @@ GLuint createShaderProgram() {
 	// read shaders from files		
 	string vertShaderStr = readShaderSource(vertShaderPath);
 	string fragShaderStr = readShaderSource(fragShaderPath);
-	const char *vertShaderSrc = vertShaderStr.c_str();
-	const char *fragShaderSrc = fragShaderStr.c_str();
+	const char* vertShaderSrc = vertShaderStr.c_str();
+	const char* fragShaderSrc = fragShaderStr.c_str();
 	glShaderSource(vShader, 1, &vertShaderSrc, NULL);
 	glShaderSource(fShader, 1, &fragShaderSrc, NULL);
 
@@ -83,18 +83,17 @@ GLuint createShaderProgram() {
 	checkOpenGLError();
 	glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);
 	if (vertCompiled != 1) {
-			cout << "vertex compilation failed" << endl;
-			printShaderLog(vShader);
+		cout << "vertex compilation failed" << endl;
+		printShaderLog(vShader);
 	}
 
 	glCompileShader(fShader);
 	checkOpenGLError();
 	glGetShaderiv(fShader, GL_COMPILE_STATUS, &fragCompiled);
 	if (fragCompiled != 1) {
-			cout << "fragment compilation failed" << endl;
-			printShaderLog(fShader);
+		cout << "fragment compilation failed" << endl;
+		printShaderLog(fShader);
 	}
-
 
 	GLuint vfProgram = glCreateProgram();
 	glAttachShader(vfProgram, vShader);
@@ -104,8 +103,8 @@ GLuint createShaderProgram() {
 	checkOpenGLError();
 	glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);
 	if (linked != 1) {
-			cout << "linking failed" << endl;
-			printProgramLog(vfProgram);
+		cout << "linking failed" << endl;
+		printProgramLog(vfProgram);
 	}
 
 	return vfProgram;
@@ -122,7 +121,7 @@ void display(GLFWwindow* window, double currentTime) {
 	glPointSize(400.0f); // un vertice es un pixel, con esto especificamos el tamanio del pixel
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); //wire frame
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // noraml
-	glDrawArrays(GL_POINTS, 0, 1);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 int main(void) {
@@ -134,17 +133,17 @@ int main(void) {
 	glfwMakeContextCurrent(window);
 
 	//if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){ exit(EXIT_FAILURE); }
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) { exit(EXIT_FAILURE); }
 
 	glfwSwapInterval(1);
 	init(window);
 	while (!glfwWindowShouldClose(window)) {
-			display(window, glfwGetTime());
-			glfwSwapBuffers(window);
-			glfwPollEvents();
+		display(window, glfwGetTime());
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
-	exit(EXIT_SUCCESS); 
+	exit(EXIT_SUCCESS);
 }
