5ff50a85e8de2c9520a52d419404da3020a1a0ec
oscar-pfuturi-h <69215265+oscar-pfuturi-h@users.noreply.github.com>
2021-07-16 10:14:56 -0500

    Create root.py

---
+++ practica_11/logarithm/root.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+import math
+
+img = cv.imread('log4.jpg', cv.IMREAD_GRAYSCALE)
+img2 = cv.imread('log4.jpg', cv.IMREAD_GRAYSCALE)
+
+print(img.shape)
+
+rows,cols=img.shape
+c=60
+
+for x in range(rows):
+    for y in range(cols):
+        img2.itemset((x,y),c*math.sqrt(img.item(x,y)))
+
+hist = cv.calcHist([img2],[0],None,[256],[0,256])
+plt.subplot(2,2,1),plt.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
+plt.subplot(2,2,2).hist(img.ravel(),256,[0,256])
+plt.subplot(2,2,3),plt.imshow(cv.cvtColor(img2, cv.COLOR_BGR2RGB))
+plt.subplot(2,2,4).hist(img2.ravel(),256,[0,256])
+plt.show()
