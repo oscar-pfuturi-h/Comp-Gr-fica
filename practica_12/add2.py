c0954a7a6ef1c1ae959842de8c99b460ba4c53bb
oscar-pfuturi-h <69215265+oscar-pfuturi-h@users.noreply.github.com>
2021-07-16 16:37:56 -0500

    Create add2.py

---
+++ practica_12/add2.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+plaza=cv.imread('plaza.jpg', cv.IMREAD_COLOR)
+leon=cv.imread('leon.jpg', cv.IMREAD_COLOR)
+
+plaza=cv.resize(plaza,(840,550),interpolation=cv.INTER_LINEAR)
+leon=cv.resize(leon,(840,550),interpolation=cv.INTER_LINEAR)
+
+print(plaza.shape,leon.shape)
+
+rows,cols,tres=plaza.shape
+
+r=0
+g=0
+b=0
+
+for x in range(rows):
+    for y in range(cols):
+        r=(plaza.item(x,y,2)+leon.item(x,y,2))/2
+        g=(plaza.item(x,y,1)+leon.item(x,y,1))/2
+        b=(plaza.item(x,y,0)+leon.item(x,y,0))/2
+        plaza.itemset((x,y,2),r)
+        plaza.itemset((x,y,1),g)
+        plaza.itemset((x,y,0),b)
+
+
+hist = cv.calcHist([plaza],[0],None,[256],[0,256])
+plt.subplot(1,2,1),plt.imshow(cv.cvtColor(plaza, cv.COLOR_BGR2RGB))
+plt.subplot(1,2,2).hist(plaza.ravel(),256,[0,256])
+plt.show()
